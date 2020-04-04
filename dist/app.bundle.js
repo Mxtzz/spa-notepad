dd76a60cb2d9abc619171b4480b5abb1f53b1dd1
ZW Ma <mazw2016@hotmail.com>
2020-04-04 20:55:50 +0800

    add header

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -91,23 +91,125 @@
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _dialog_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2);
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_style_css__WEBPACK_IMPORTED_MODULE_1__);
 
 
-function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
 
-    element.innerHTML = "This is Notepad!";
+class Main {
+    constructor() {
+        this.headerData = [
+            {
+                ID: 1,
+                MenuItem: "文件(F)",
+                List: [
+                    { Name: "新建(N)" },
+                    { Name: "新窗口(W)" },
+                    { Name: "打开(O)..." },
+                    { Name: "保存(S)" },
+                    { Name: "另存为(A)..." },
+                    { Name: "页面设置(U)..." },
+                    { Name: "打印(P)..." },
+                    { Name: "退出(X)" }
+                ]
+            }, {
+                ID: 2,
+                MenuItem: "编辑(E)",
+                List: [
+                    { Name: "撤销" },
+                    { Name: "剪切(W)" },
+                    { Name: "复制(O)" },
+                    { Name: "粘贴(S)" },
+                    { Name: "删除(A)" },
+                    { Name: "使用Bing搜索(U)..." },
+                    { Name: "查找(P)..." },
+                    { Name: "查找下一个(X)" },
+                    { Name: "查找上一个(X)" },
+                    { Name: "替换()" },
+                    { Name: "转到()" },
+                    { Name: "全选()" },
+                    { Name: "时间/日期(D)" },
+                ]
+            }, {
+                ID: 3,
+                MenuItem: "格式(O)",
+                List: [
+                    { Name: "自动换行()" },
+                    { Name: "字体()" }
+                ]
+            }, {
+                ID: 4,
+                MenuItem: "查看(V)",
+                List: [
+                    { Name: "缩放(N)" },
+                    { Name: "状态栏()" }
+                ]
+            }, {
+                ID: 5,
+                MenuItem: "帮助(H)",
+                List: [
+                    { Name: "查看帮助()" },
+                    { Name: "发送反馈()" },
+                    { Name: "关于记事本()" }
+                ]
+            }
+        ]
+    }
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = _dialog_js__WEBPACK_IMPORTED_MODULE_0__["default"];
+    initPage() {
+        const me = this;
+        const element = document.createElement('div');
+        element.classList.add("text");
+        element.innerHTML = `
+            <div class="header">
+                <div class="title">
+                    <div class="title-left">新建文本文档.txt</div>
+                    <div class="title-right">
+                        <span class="min">-</span>
+                        <span class="max">□</span>
+                        <span class="close">×</span>
+                    </div>
+                </div>
+            </div>
+            <div class="container"></div>
+            <div class="footer"></div>
+        `;
 
-    element.appendChild(btn);
+        element.querySelector(".header").appendChild(me.renderHeader(me.headerData));
 
-    return element;
+        return element;
+    }
+
+    renderHeader(data) {
+        const me = this;
+        const menuElement = document.createElement("div");
+        menuElement.classList.add("menu");
+
+        for (let i = 0; i < data.length; i ++) {
+            const menuItem = data[i];
+            const menuItemHtml = document.createElement("span");
+            menuItemHtml.classList.add("menu-item");
+            menuItemHtml.innerText = menuItem.MenuItem;
+            const menuItemContainerHtml = document.createElement("ul");
+            menuItemContainerHtml.classList.add("menu-list-container");
+
+            for (let j = 0; j < menuItem.List.length; j ++) {
+                const childListItem = menuItem.List[j];
+                const tmpLi = document.createElement("li");
+                tmpLi.innerHTML = childListItem.Name;
+                menuItemContainerHtml.appendChild(tmpLi);
+            }
+
+            menuItemHtml.appendChild(menuItemContainerHtml);
+            menuElement.appendChild(menuItemHtml);
+        }
+
+        return menuElement;
+    }
 }
 
-document.body.appendChild(component());
+const newMain = new Main();
+document.body.appendChild(newMain.initPage());
 
 /***/ }),
 /* 1 */
@@ -119,6 +221,420 @@ __webpack_require__.r(__webpack_exports__);
 function dialog() {
     console.log("This is a dialog of notepad!");
   }
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var api = __webpack_require__(3);
+            var content = __webpack_require__(4);
+
+            content = content.__esModule ? content.default : content;
+
+            if (typeof content === 'string') {
+              content = [[module.i, content, '']];
+            }
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = api(content, options);
+
+var exported = content.locals ? content.locals : {};
+
+
+
+module.exports = exported;
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var isOldIE = function isOldIE() {
+  var memo;
+  return function memorize() {
+    if (typeof memo === 'undefined') {
+      // Test for IE <= 9 as proposed by Browserhacks
+      // @see http://browserhacks.com/#hack-e71d8692f65334173fee715c222cb805
+      // Tests for existence of standard globals is to allow style-loader
+      // to operate correctly into non-standard environments
+      // @see https://github.com/webpack-contrib/style-loader/issues/177
+      memo = Boolean(window && document && document.all && !window.atob);
+    }
+
+    return memo;
+  };
+}();
+
+var getTarget = function getTarget() {
+  var memo = {};
+  return function memorize(target) {
+    if (typeof memo[target] === 'undefined') {
+      var styleTarget = document.querySelector(target); // Special case to return head of iframe instead of iframe itself
+
+      if (window.HTMLIFrameElement && styleTarget instanceof window.HTMLIFrameElement) {
+        try {
+          // This will throw an exception if access to iframe is blocked
+          // due to cross-origin restrictions
+          styleTarget = styleTarget.contentDocument.head;
+        } catch (e) {
+          // istanbul ignore next
+          styleTarget = null;
+        }
+      }
+
+      memo[target] = styleTarget;
+    }
+
+    return memo[target];
+  };
+}();
+
+var stylesInDom = [];
+
+function getIndexByIdentifier(identifier) {
+  var result = -1;
+
+  for (var i = 0; i < stylesInDom.length; i++) {
+    if (stylesInDom[i].identifier === identifier) {
+      result = i;
+      break;
+    }
+  }
+
+  return result;
+}
+
+function modulesToDom(list, options) {
+  var idCountMap = {};
+  var identifiers = [];
+
+  for (var i = 0; i < list.length; i++) {
+    var item = list[i];
+    var id = options.base ? item[0] + options.base : item[0];
+    var count = idCountMap[id] || 0;
+    var identifier = "".concat(id, " ").concat(count);
+    idCountMap[id] = count + 1;
+    var index = getIndexByIdentifier(identifier);
+    var obj = {
+      css: item[1],
+      media: item[2],
+      sourceMap: item[3]
+    };
+
+    if (index !== -1) {
+      stylesInDom[index].references++;
+      stylesInDom[index].updater(obj);
+    } else {
+      stylesInDom.push({
+        identifier: identifier,
+        updater: addStyle(obj, options),
+        references: 1
+      });
+    }
+
+    identifiers.push(identifier);
+  }
+
+  return identifiers;
+}
+
+function insertStyleElement(options) {
+  var style = document.createElement('style');
+  var attributes = options.attributes || {};
+
+  if (typeof attributes.nonce === 'undefined') {
+    var nonce =  true ? __webpack_require__.nc : undefined;
+
+    if (nonce) {
+      attributes.nonce = nonce;
+    }
+  }
+
+  Object.keys(attributes).forEach(function (key) {
+    style.setAttribute(key, attributes[key]);
+  });
+
+  if (typeof options.insert === 'function') {
+    options.insert(style);
+  } else {
+    var target = getTarget(options.insert || 'head');
+
+    if (!target) {
+      throw new Error("Couldn't find a style target. This probably means that the value for the 'insert' parameter is invalid.");
+    }
+
+    target.appendChild(style);
+  }
+
+  return style;
+}
+
+function removeStyleElement(style) {
+  // istanbul ignore if
+  if (style.parentNode === null) {
+    return false;
+  }
+
+  style.parentNode.removeChild(style);
+}
+/* istanbul ignore next  */
+
+
+var replaceText = function replaceText() {
+  var textStore = [];
+  return function replace(index, replacement) {
+    textStore[index] = replacement;
+    return textStore.filter(Boolean).join('\n');
+  };
+}();
+
+function applyToSingletonTag(style, index, remove, obj) {
+  var css = remove ? '' : obj.media ? "@media ".concat(obj.media, " {").concat(obj.css, "}") : obj.css; // For old IE
+
+  /* istanbul ignore if  */
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = replaceText(index, css);
+  } else {
+    var cssNode = document.createTextNode(css);
+    var childNodes = style.childNodes;
+
+    if (childNodes[index]) {
+      style.removeChild(childNodes[index]);
+    }
+
+    if (childNodes.length) {
+      style.insertBefore(cssNode, childNodes[index]);
+    } else {
+      style.appendChild(cssNode);
+    }
+  }
+}
+
+function applyToTag(style, options, obj) {
+  var css = obj.css;
+  var media = obj.media;
+  var sourceMap = obj.sourceMap;
+
+  if (media) {
+    style.setAttribute('media', media);
+  } else {
+    style.removeAttribute('media');
+  }
+
+  if (sourceMap && btoa) {
+    css += "\n/*# sourceMappingURL=data:application/json;base64,".concat(btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap)))), " */");
+  } // For old IE
+
+  /* istanbul ignore if  */
+
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    while (style.firstChild) {
+      style.removeChild(style.firstChild);
+    }
+
+    style.appendChild(document.createTextNode(css));
+  }
+}
+
+var singleton = null;
+var singletonCounter = 0;
+
+function addStyle(obj, options) {
+  var style;
+  var update;
+  var remove;
+
+  if (options.singleton) {
+    var styleIndex = singletonCounter++;
+    style = singleton || (singleton = insertStyleElement(options));
+    update = applyToSingletonTag.bind(null, style, styleIndex, false);
+    remove = applyToSingletonTag.bind(null, style, styleIndex, true);
+  } else {
+    style = insertStyleElement(options);
+    update = applyToTag.bind(null, style, options);
+
+    remove = function remove() {
+      removeStyleElement(style);
+    };
+  }
+
+  update(obj);
+  return function updateStyle(newObj) {
+    if (newObj) {
+      if (newObj.css === obj.css && newObj.media === obj.media && newObj.sourceMap === obj.sourceMap) {
+        return;
+      }
+
+      update(obj = newObj);
+    } else {
+      remove();
+    }
+  };
+}
+
+module.exports = function (list, options) {
+  options = options || {}; // Force single-tag solution on IE6-9, which has a hard limit on the # of <style>
+  // tags it will allow on a page
+
+  if (!options.singleton && typeof options.singleton !== 'boolean') {
+    options.singleton = isOldIE();
+  }
+
+  list = list || [];
+  var lastIdentifiers = modulesToDom(list, options);
+  return function update(newList) {
+    newList = newList || [];
+
+    if (Object.prototype.toString.call(newList) !== '[object Array]') {
+      return;
+    }
+
+    for (var i = 0; i < lastIdentifiers.length; i++) {
+      var identifier = lastIdentifiers[i];
+      var index = getIndexByIdentifier(identifier);
+      stylesInDom[index].references--;
+    }
+
+    var newLastIdentifiers = modulesToDom(newList, options);
+
+    for (var _i = 0; _i < lastIdentifiers.length; _i++) {
+      var _identifier = lastIdentifiers[_i];
+
+      var _index = getIndexByIdentifier(_identifier);
+
+      if (stylesInDom[_index].references === 0) {
+        stylesInDom[_index].updater();
+
+        stylesInDom.splice(_index, 1);
+      }
+    }
+
+    lastIdentifiers = newLastIdentifiers;
+  };
+};
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(5);
+exports = ___CSS_LOADER_API_IMPORT___(false);
+// Module
+exports.push([module.i, "* {\r\n    padding: 0;\r\n    margin: 0;\r\n    user-select:none;\r\n}\r\n\r\nbody {\r\n    background: #333;\r\n}\r\n\r\nul {\r\n    \r\n}\r\n\r\nli {\r\n    list-style: none;\r\n}\r\n\r\n.text {\r\n    height: 600px;\r\n    width: 660px;\r\n    margin: 20px;\r\n    border: 1px solid #666;\r\n    position: relative;\r\n    font-size: 12px;\r\n    background: #fff;\r\n}\r\n\r\n.header {\r\n    border-bottom: 1.5px solid rgb(240, 240, 240);\r\n}\r\n\r\n.title {\r\n    display: flex;\r\n    justify-content: space-between;\r\n    flex: 1;\r\n}\r\n\r\n.title-left {\r\n    font-size: 12px;\r\n    padding: 6px;\r\n}\r\n\r\n.title-right {\r\n    display: flex;\r\n    font-size: 20px;\r\n}\r\n\r\n.title-right span{\r\n    height: 28px;\r\n    width: 50px;\r\n    align-items: center;\r\n    display: flex;\r\n    justify-content: center;\r\n    transition: background-color 200ms ease-in-out;\r\n}\r\n\r\n.title-right span:hover {\r\n    background: rgb(229, 229, 229);\r\n}\r\n\r\n.title-right .close:hover {\r\n    background: rgb(232, 17, 35);\r\n    color: rgb(240, 240, 240);\r\n}\r\n\r\n.menu {\r\n    display: flex;\r\n}\r\n\r\n.menu-item{\r\n    padding: 3px 5px;\r\n    border: 1px solid rgb(255, 255, 255);\r\n    position: relative;\r\n}\r\n\r\n.menu-item:hover {\r\n    background: rgb(229, 243, 255);\r\n    border: 1px solid rgb(204, 232, 255);\r\n}\r\n\r\n.menu-list-container {\r\n    position: absolute;\r\n    background: rgb(242, 242, 242);\r\n    width: 200px;\r\n    padding: 2px;\r\n    border: 1px solid #eee;\r\n    box-shadow: 2px 2px 3px #aaa;\r\n    top: 23px;\r\n    left: 0;\r\n}\r\n\r\n.menu-list-item {\r\n    padding: 2px 22px;\r\n}\r\n\r\n.menu-list-item:hover{\r\n    background: rgb(145,201,247);\r\n}\r\n\r\n.container {\r\n\r\n}\r\n\r\n.footer {\r\n    position: absolute;\r\n    left: 0;\r\n    right: 0;\r\n    bottom: 0;\r\n    height: 24px;\r\n    background: rgb(240, 240, 240);\r\n}\r\n", ""]);
+// Exports
+module.exports = exports;
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/*
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Tobias Koppers @sokra
+*/
+// css base code, injected by the css-loader
+// eslint-disable-next-line func-names
+module.exports = function (useSourceMap) {
+  var list = []; // return the list of modules as css string
+
+  list.toString = function toString() {
+    return this.map(function (item) {
+      var content = cssWithMappingToString(item, useSourceMap);
+
+      if (item[2]) {
+        return "@media ".concat(item[2], " {").concat(content, "}");
+      }
+
+      return content;
+    }).join('');
+  }; // import a list of modules into the list
+  // eslint-disable-next-line func-names
+
+
+  list.i = function (modules, mediaQuery, dedupe) {
+    if (typeof modules === 'string') {
+      // eslint-disable-next-line no-param-reassign
+      modules = [[null, modules, '']];
+    }
+
+    var alreadyImportedModules = {};
+
+    if (dedupe) {
+      for (var i = 0; i < this.length; i++) {
+        // eslint-disable-next-line prefer-destructuring
+        var id = this[i][0];
+
+        if (id != null) {
+          alreadyImportedModules[id] = true;
+        }
+      }
+    }
+
+    for (var _i = 0; _i < modules.length; _i++) {
+      var item = [].concat(modules[_i]);
+
+      if (dedupe && alreadyImportedModules[item[0]]) {
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+
+      if (mediaQuery) {
+        if (!item[2]) {
+          item[2] = mediaQuery;
+        } else {
+          item[2] = "".concat(mediaQuery, " and ").concat(item[2]);
+        }
+      }
+
+      list.push(item);
+    }
+  };
+
+  return list;
+};
+
+function cssWithMappingToString(item, useSourceMap) {
+  var content = item[1] || ''; // eslint-disable-next-line prefer-destructuring
+
+  var cssMapping = item[3];
+
+  if (!cssMapping) {
+    return content;
+  }
+
+  if (useSourceMap && typeof btoa === 'function') {
+    var sourceMapping = toComment(cssMapping);
+    var sourceURLs = cssMapping.sources.map(function (source) {
+      return "/*# sourceURL=".concat(cssMapping.sourceRoot || '').concat(source, " */");
+    });
+    return [content].concat(sourceURLs).concat([sourceMapping]).join('\n');
+  }
+
+  return [content].join('\n');
+} // Adapted from convert-source-map (MIT)
+
+
+function toComment(sourceMap) {
+  // eslint-disable-next-line no-undef
+  var base64 = btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap))));
+  var data = "sourceMappingURL=data:application/json;charset=utf-8;base64,".concat(base64);
+  return "/*# ".concat(data, " */");
+}
 
 /***/ })
 /******/ ]);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,117 @@
 import dialog from './dialog.js';
+import './style.css';
 
-function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
+class Main {
+    constructor() {
+        this.headerData = [
+            {
+                ID: 1,
+                MenuItem: "文件(F)",
+                List: [
+                    { Name: "新建(N)" },
+                    { Name: "新窗口(W)" },
+                    { Name: "打开(O)..." },
+                    { Name: "保存(S)" },
+                    { Name: "另存为(A)..." },
+                    { Name: "页面设置(U)..." },
+                    { Name: "打印(P)..." },
+                    { Name: "退出(X)" }
+                ]
+            }, {
+                ID: 2,
+                MenuItem: "编辑(E)",
+                List: [
+                    { Name: "撤销" },
+                    { Name: "剪切(W)" },
+                    { Name: "复制(O)" },
+                    { Name: "粘贴(S)" },
+                    { Name: "删除(A)" },
+                    { Name: "使用Bing搜索(U)..." },
+                    { Name: "查找(P)..." },
+                    { Name: "查找下一个(X)" },
+                    { Name: "查找上一个(X)" },
+                    { Name: "替换()" },
+                    { Name: "转到()" },
+                    { Name: "全选()" },
+                    { Name: "时间/日期(D)" },
+                ]
+            }, {
+                ID: 3,
+                MenuItem: "格式(O)",
+                List: [
+                    { Name: "自动换行()" },
+                    { Name: "字体()" }
+                ]
+            }, {
+                ID: 4,
+                MenuItem: "查看(V)",
+                List: [
+                    { Name: "缩放(N)" },
+                    { Name: "状态栏()" }
+                ]
+            }, {
+                ID: 5,
+                MenuItem: "帮助(H)",
+                List: [
+                    { Name: "查看帮助()" },
+                    { Name: "发送反馈()" },
+                    { Name: "关于记事本()" }
+                ]
+            }
+        ]
+    }
 
-    element.innerHTML = "This is Notepad!";
+    initPage() {
+        const me = this;
+        const element = document.createElement('div');
+        element.classList.add("text");
+        element.innerHTML = `
+            <div class="header">
+                <div class="title">
+                    <div class="title-left">新建文本文档.txt</div>
+                    <div class="title-right">
+                        <span class="min">-</span>
+                        <span class="max">□</span>
+                        <span class="close">×</span>
+                    </div>
+                </div>
+            </div>
+            <div class="container"></div>
+            <div class="footer"></div>
+        `;
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = dialog;
+        element.querySelector(".header").appendChild(me.renderHeader(me.headerData));
 
-    element.appendChild(btn);
+        return element;
+    }
 
-    return element;
+    renderHeader(data) {
+        const me = this;
+        const menuElement = document.createElement("div");
+        menuElement.classList.add("menu");
+
+        for (let i = 0; i < data.length; i ++) {
+            const menuItem = data[i];
+            const menuItemHtml = document.createElement("span");
+            menuItemHtml.classList.add("menu-item");
+            menuItemHtml.innerText = menuItem.MenuItem;
+            const menuItemContainerHtml = document.createElement("ul");
+            menuItemContainerHtml.classList.add("menu-list-container");
+
+            for (let j = 0; j < menuItem.List.length; j ++) {
+                const childListItem = menuItem.List[j];
+                const tmpLi = document.createElement("li");
+                tmpLi.innerHTML = childListItem.Name;
+                menuItemContainerHtml.appendChild(tmpLi);
+            }
+
+            menuItemHtml.appendChild(menuItemContainerHtml);
+            menuElement.appendChild(menuItemHtml);
+        }
+
+        return menuElement;
+    }
 }
 
-document.body.appendChild(component());
+const newMain = new Main();
+document.body.appendChild(newMain.initPage());
